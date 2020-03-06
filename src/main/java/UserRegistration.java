ae33a56f992fc725ba51d1dbb607d6b80890e132
rahulgidde <rahulgidde1212@gmail.com>
2020-03-06 09:14:33 +0530

    ADDED:Validate Email Address

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration
     //DECLARE PATTERN
     String firstNamePattern="^[A-Z][a-zA-Z]{2,}$";
     String lastNamePattern="^[A-Z][a-zA-Z]{2,}$";
+    String emailPattern="^[a-z]{3}((([.+_-]?+[a-zA-z-0-9])+([@]+[a-zA-Z0-9]+))|[@][a-zA-Z0-9]+)[.][a-z]{3}[.]?([a-z]{2,4}|[a-z]?)$";
     public static void main(String[] args)
     {
         System.out.println("-----WELCOME TO USER REGISTRATION-----");
@@ -22,4 +23,9 @@ public class UserRegistration
         return (Pattern.matches(lastNamePattern,lastName));
     }
 
+    //CHECK PATTERN FOR EMAIL ADDRESS
+    public boolean validateEmailAddress(String email)
+    {
+        return (Pattern.matches(emailPattern,email));
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -5,6 +5,13 @@ public class UserRegistrationTest
 {
     UserRegistration validate=new UserRegistration();
 
+    String[]validEmail={"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com",
+                        "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
+                        "abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
+    String[]invalidEmail={"abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",
+                          ".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com",
+                          "abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+
     //TEST CASE TO CHECK FIRST NAME IS VALID
     @Test
     public void givenFirstName_WhenValid_thenReturn()
@@ -39,5 +46,29 @@ public class UserRegistrationTest
         String lastName="gidde";
         boolean result=validate.validateLastName(lastName);
         Assert.assertFalse(result);
+    }
+
+    //TEST CASE TO CHECK EMAIL ADDRESS IS VALID
+    @Test
+    public void givenEmailAddress_WhenValid_thenReturn()
+    {
+        for(int index=0; index<validEmail.length; index++)
+        {
+            String email = validEmail[index];
+            boolean result = validate.validateEmailAddress(email);
+            Assert.assertTrue(result);
+        }
+    }
+
+    //TEST CASE TO CHECK EMAIL ADDRESS IS INVALID
+    @Test
+    public void givenEmailAddress_WhenInvalid_thenReturn()
+    {
+        for(int index=0; index<invalidEmail.length; index++)
+        {
+            String email = invalidEmail[index];
+            boolean result = validate.validateEmailAddress(email);
+            Assert.assertFalse(result);
+        }
     }
 }
