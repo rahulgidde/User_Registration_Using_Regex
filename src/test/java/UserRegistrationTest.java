2346d4eb3fd72220c090280ff3813d5aa1b68d46
rahulgidde <rahulgidde1212@gmail.com>
2020-03-09 10:01:11 +0530

    Modified:Changes Variable Names

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -3,7 +3,8 @@ import org.junit.Test;
 
 public class UserRegistrationTest
 {
-    UserRegistration validate=new UserRegistration();
+    boolean result;
+    UserRegistration validate = new UserRegistration();
 
     String[]validEmail={"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com",
                         "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
@@ -14,151 +15,151 @@ public class UserRegistrationTest
 
     //TEST CASE TO CHECK FIRST NAME IS VALID
     @Test
-    public void givenFirstName_whenValid_thenReturn()
+    public void givenFirstName_WhenValid_ThenReturnTrue()
     {
         String firstName="Rahul";
-        boolean result=validate.validateFirstName(firstName);
+        result = validate.validateFirstName(firstName);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK FIRST NAME IS INVALID
     @Test
-    public void givenFirstName_whenInvalid_thenReturn()
+    public void givenFirstName_WhenInvalid_ThenReturnFalse()
     {
         String firstName="rahul";
-        boolean result=validate.validateFirstName(firstName);
+        result = validate.validateFirstName(firstName);
         Assert.assertFalse(result);
     }
 
     //TEST CASE TO CHECK LAST NAME IS VALID
     @Test
-    public void givenLastName_whenValid_thenReturn()
+    public void givenLastName_WhenValid_ThenReturnTrue()
     {
         String lastName="Gidde";
-        boolean result=validate.validateLastName(lastName);
+        result = validate.validateLastName(lastName);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK LAST NAME IS INVALID
     @Test
-    public void givenLastName_whenInvalid_thenReturn()
+    public void givenLastName_WhenInvalid_ThenReturnFalse()
     {
         String lastName="gidde";
-        boolean result=validate.validateLastName(lastName);
+        result = validate.validateLastName(lastName);
         Assert.assertFalse(result);
     }
 
     //TEST CASE TO CHECK EMAIL ADDRESS IS VALID
     @Test
-    public void givenEmailAddress_whenValid_thenReturn()
+    public void givenEmailAddress_WhenValid_ThenReturnTrue()
     {
         for(int index=0; index<validEmail.length; index++)
         {
             String email = validEmail[index];
-            boolean result = validate.validateEmailAddress(email);
+            result = validate.validateEmailAddress(email);
             Assert.assertTrue(result);
         }
     }
 
     //TEST CASE TO CHECK EMAIL ADDRESS IS INVALID
     @Test
-    public void givenEmailAddress_whenInvalid_thenReturn()
+    public void givenEmailAddress_WhenInvalid_ThenReturnFalse()
     {
         for(int index=0; index<invalidEmail.length; index++)
         {
             String email = invalidEmail[index];
-            boolean result = validate.validateEmailAddress(email);
+            result = validate.validateEmailAddress(email);
             Assert.assertFalse(result);
         }
     }
 
     //TEST CASE TO CHECK MOBILE NUMBER IS VALID
     @Test
-    public void givenNumber_whenValid_thenReturn()
+    public void givenNumber_WhenValid_ThenReturnTrue()
     {
         String number="+91 9665382289";
-        boolean result=validate.validateNumber(number);
+        result = validate.validateNumber(number);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK MOBILE NUMBER IS INVALID
     @Test
-    public void givenNumber_whenInValid_thenReturn()
+    public void givenNumber_WhenInValid_ThenReturnFalse()
     {
         String number="9665382289";
-        boolean result=validate.validateNumber(number);
+        result = validate.validateNumber(number);
         Assert.assertFalse(result);
     }
 
     //TEST CASE TO CHECK MINIMUM 8 CHARACTERS PASSWORD IS VALID
     @Test
-    public void givenPasswordRule1_whenValid_thenReturn()
+    public void givenPasswordRule1_WhenValid_ThenReturnTrue()
     {
         String password="Aafsdvgsaghb1*1";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK MINIMUM 8 CHARACTERS PASSWORD IS INVALID
     @Test
-    public void givenPasswordRule1_whenInvalid_thenReturn()
+    public void givenPasswordRule1_WhenInvalid_ThenReturnFalse()
     {
         String password="asda";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertFalse(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE UPPER CASE CHARACTER
     @Test
-    public void givenPasswordRule2_whenValid_thenReturn()
+    public void givenPasswordRule2_WhenValid_ThenReturnTrue()
     {
         String password="adadaAaxaa$4";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertTrue(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE UPPER CASE CHARACTER
     @Test
-    public void givenPasswordRule2_whenInvalid_thenReturn()
+    public void givenPasswordRule2_WhenInvalid_ThenReturnFalse()
     {
         String password="asdasdfafa";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertFalse(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE NUMERIC NUMBER
     @Test
-    public void givenPasswordRule3_whenValid_thenReturn()
+    public void givenPasswordRule3_WhenValid_ThenReturnTrue()
     {
         String password="adadaAaxa11a#2";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertTrue(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE NUMERIC
     @Test
-    public void givenPasswordRule3_whenInvalid_thenReturn()
+    public void givenPasswordRule3_WhenInvalid_ThenReturnFalse()
     {
         String password="asdasdfafa";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertFalse(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE SPECIAL CHARACTER
     @Test
-    public void givenPasswordRule4_whenValid_thenReturn()
+    public void givenPasswordRule4_WhenValid_ThenReturnTrue()
     {
         String password="adadaAaxa11a2@";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertTrue(result);
     }
 
     //TEST CASE FOR VALID PASSWORD FOR AT LEAST ONE SPECIAL CHARACTER
     @Test
-    public void givenPasswordRule4_whenInvalid_thenReturn()
+    public void givenPasswordRule4_WhenInvalid_ThenReturnFalse()
     {
         String password="asdasdfafa4A";
-        boolean result=validate.validatePassword(password);
+        result = validate.validatePassword(password);
         Assert.assertFalse(result);
     }
 }
